5996a7416b71292ac17200fabd2abe132f348c5e
Alexander Ryndin <ryndin.as@gmail.com>
2013-04-13 20:28:45 +0300

    initial commit

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Chat.Hubs
+{
+  [HubName("chat")]
+  public class ChatHub : Hub
+  {
+    public void Send(string message)
+    {
+      Clients.Others.receiveMessage(message);
+    }
+  }
+}
